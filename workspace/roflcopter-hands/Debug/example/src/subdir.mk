86ba455cf9d7f3d83436ae8fa9d4c65ed2110dd4
SeanMcLoughlin <smcloughlin1015@gmail.com>
2017-03-26 16:01:34 -0400

    Minor error in the example that I posted yesterday, renamed the main file to main.c

--- workspace/roflcopter-hands/Debug/example/src/subdir.mk
+++ workspace/roflcopter-hands/Debug/example/src/subdir.mk
@@ -5,25 +5,25 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../example/src/cr_startup_lpc175x_6x.c \
-../example/src/sysinit.c \
-../example/src/uart_rb.c 
+../example/src/main.c \
+../example/src/sysinit.c 
 
 OBJS += \
 ./example/src/cr_startup_lpc175x_6x.o \
-./example/src/sysinit.o \
-./example/src/uart_rb.o 
+./example/src/main.o \
+./example/src/sysinit.o 
 
 C_DEPS += \
 ./example/src/cr_startup_lpc175x_6x.d \
-./example/src/sysinit.d \
-./example/src/uart_rb.d 
+./example/src/main.d \
+./example/src/sysinit.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 example/src/%.o: ../example/src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -D__REDLIB__ -DDEBUG -D__CODE_RED -D__USE_LPCOPEN -DCORE_M3 -I"/home/smclough/LPCXpresso/workspace/lpc_chip_175x_6x/inc" -I"/home/smclough/LPCXpresso/workspace/lpc_board_nxp_lpcxpresso_1769/inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m3 -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-none-eabi-gcc -D__REDLIB__ -DDEBUG -D__CODE_RED -D__USE_LPCOPEN -DCORE_M3 -I"/home/smclough/Documents/university/teamroflcopter-373/workspace/lpc_chip_175x_6x/inc" -I"/home/smclough/Documents/university/teamroflcopter-373/workspace/lpc_board_nxp_lpcxpresso_1769/inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m3 -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
