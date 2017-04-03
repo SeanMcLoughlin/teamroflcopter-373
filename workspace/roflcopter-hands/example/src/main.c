2d958d20db1d76ebc36998d50466d01a0f801c57
Gordon Dwyer <Gordon Dwyer>
2017-04-03 19:57:46 -0400

    Linear Mapping seems to be working

--- workspace/roflcopter-hands/example/src/main.c
+++ workspace/roflcopter-hands/example/src/main.c
@@ -8,6 +8,7 @@
 #include "string.h"
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
 
 
 // Select UART Handlers
@@ -72,7 +73,8 @@ void getADCSample(uint8_t* data, uint8_t sample) {
 			// Concatenate the two samples so that you can multiply by 4 (12 bit DAC vs 10 bit sample)
 			uint16_t xtemp = byteConcat(x_axis[0], x_axis[1]);
 			xtemp *= 4;
-
+			//if (xtemp > (4095 * ceil(2.1/3.3))) xtemp = 4095 * ceil(2.1/3.3);
+			xtemp = (xtemp - ceil(4095 * (0.4/3.3)))*ceil(3.3/2.4);
 			// Put the sample back into the indices
 			x_axis[0] = (xtemp & 0xFF00) >> 8;
 			x_axis[1] = (xtemp & 0x00FF);
@@ -83,9 +85,9 @@ void getADCSample(uint8_t* data, uint8_t sample) {
 			y_axis[1] = data[22];
 
 			uint16_t ytemp = byteConcat(y_axis[0], y_axis[1]);
-
 			ytemp *= 4;
-
+			//if (ytemp > (4095 * ceil(2.1/3.3))) ytemp = 4095 * ceil(2.1/3.3);
+			ytemp = (ytemp - ceil(4095 * (0.4/3.3)))*ceil(3.3/2.4);
 			y_axis[0] = (ytemp & 0xFF00) >> 8;
 			y_axis[1] = (ytemp & 0x00FF);
 			break;
@@ -236,12 +238,13 @@ int main(void)
 	NVIC_SetPriority(IRQ_SELECTION, 1);
 	NVIC_EnableIRQ(IRQ_SELECTION);
 
+	uint8_t latch[24]; // The latched value of data (since we are shifting data in)
+
 	while(1) {
 		// First word is always 0x7E00, next is the size of packet, next is 0x82 (for some reason)
 
 		if (data[0] == 0x7E && data[1] == 0x00 && data[2] == 0x14 && data[3] == 0x82) {
 
-			uint8_t latch[24];
 			int i;
 			for (i = 0; i < 24; i++)
 			{
@@ -270,11 +273,25 @@ int main(void)
 				tmp = Chip_I2C_MasterSend(I2C0, DAC_ADDRESS_1, y_axis, 2);
 			}
 		}
+		else {
+			// Output samples on I2C for the right accelerometer
+			if (xbeeAddress(latch) == RIGHT_HAND_ADDR) {
+				Chip_I2C_SetMasterEventHandler(I2C1, Chip_I2C_EventHandler);
+				int tmp = Chip_I2C_MasterSend(I2C1, DAC_ADDRESS_0, x_axis, 2);
+				//assert(tmp == 2); // To ensure that both bytes are transferred
+
+				tmp = Chip_I2C_MasterSend(I2C1, DAC_ADDRESS_1, y_axis, 2);
+				//assert(tmp == 2); // To ensure that both bytes are transferred
+			}
+
+			// Output samples on I2C for the left accelerometer
+			// TODO: I2C1 is not working. Only I2C0
+			else if (xbeeAddress(latch) == LEFT_HAND_ADDR) {
+				Chip_I2C_SetMasterEventHandler(I2C0, Chip_I2C_EventHandler);
+				int tmp = Chip_I2C_MasterSend(I2C0, DAC_ADDRESS_0, x_axis, 2);
+				tmp = Chip_I2C_MasterSend(I2C0, DAC_ADDRESS_1, y_axis, 2);
+			}
+		}
 	}
 	return 1;
 }
-
-// Questions:
-// 1. Are interrupts ever occurring when you're in the ISR?
-// 2. How long is the ISR actually taking?
-// 3. Try to read out a shorter sequence each time (4 bytes) ... but probably not the problem
